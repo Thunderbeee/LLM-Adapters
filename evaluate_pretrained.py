75978c78d29a1d814ee5ac77022f4b625f3823ba
Mingyuan Ma <mingyuan_ma@g.harvard.edu>
2023-09-13 19:20:23 -0700

    evaluate

--- evaluate_pretrained.py
+++ evaluate_pretrained.py
@@ -10,6 +10,7 @@ import fire
 import torch
 
 sys.path.append(os.path.join(os.getcwd(), "peft/src/"))
+
 from peft import PeftModel
 from tqdm import tqdm
 from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer, AutoModelForCausalLM, AutoTokenizer
@@ -19,17 +20,11 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
-try:
-    if torch.backends.mps.is_available():
-        device = "mps"
-except:  # noqa: E722
-    pass
 
 
 def main(
         load_8bit: bool = False,
         base_model: str = "",
-        lora_weights: str = "tloen/alpaca-lora-7b",
         share_gradio: bool = False,
 ):
     args = parse_args()
@@ -55,14 +50,6 @@ def main(
             **kwargs,
         )
         with torch.no_grad():
-            # generation_output = model.generate(
-            #     input_ids=input_ids,
-            #     generation_config=generation_config,
-            #     return_dict_in_generate=True,
-            #     output_scores=True,
-            #     max_new_tokens=max_new_tokens,
-            #     use_cache=False,
-            # )
             generation_output = model.generate(
                 input_ids=input_ids,
                 generation_config=generation_config,
@@ -73,24 +60,7 @@ def main(
         output = tokenizer.decode(s)
         return output.split("### Response:")[1].strip()
 
-    """
-    # testing code for readme
-    for instruction in [
-        "Tell me about alpacas.",
-        "Tell me about the president of Mexico in 2019.",
-        "Tell me about the king of France in 2019.",
-        "List all Canadian provinces in alphabetical order.",
-        "Write a Python program that prints the first 10 Fibonacci numbers.",
-        "Write a program that prints the numbers from 1 to 100. But for multiples of three print 'Fizz' instead of the number and for the multiples of five print 'Buzz'. For numbers which are multiples of both three and five print 'FizzBuzz'.",  # noqa: E501
-        "Tell me five words that rhyme with 'shock'.",
-        "Translate the sentence 'I have no mouth but I must scream' into Spanish.",
-        "Count up from 1 to 500.",
-    ]:
-        print("Instruction:", instruction)
-        print("Response:", evaluate(instruction))
-        print()
-    """
-    save_file = f'experiment/{args.model}-{args.adapter}-{args.dataset}.json'
+    save_file = f'experiment/{args.model}-{args.dataset}.json'
     create_dir('experiment/')
 
     dataset = load_data(args)
@@ -102,6 +72,7 @@ def main(
     pbar = tqdm(total=total)
     for idx, data in enumerate(dataset):
         instruction = data.get('instruction')
+
         outputs = evaluate(instruction)
         label = data.get('answer')
         flag = False
@@ -185,11 +156,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', choices=['AddSub', 'MultiArith', 'SingleEq', 'gsm8k', 'AQuA', 'SVAMP'],
                         required=True)
-    parser.add_argument('--model', choices=['LLaMA-7B', 'BLOOM-7B', 'GPT-j-6B', 'LLaMA-13B'], required=True)
-    parser.add_argument('--adapter', choices=['LoRA', 'AdapterP', 'AdapterH', 'Parallel', 'Prefix'],
-                        required=True)
+    parser.add_argument('--model', choices=['LLaMA-7B', 'LLaMA2-7B', 'LLaMA2-13B', 'BLOOM-7B', 'GPT-j-6B'], required=True)
     parser.add_argument('--base_model', required=True)
-    parser.add_argument('--lora_weights', required=True)
     parser.add_argument('--load_8bit', action='store_true', default=False)
 
     return parser.parse_args()
@@ -205,51 +173,31 @@ def load_model(args) -> tuple:
         tuple(tokenizer, model)
     """
     base_model = args.base_model
-    if not base_model:
-        raise ValueError(f'can not find base model name by the value: {args.model}')
-    lora_weights = args.lora_weights
-    if not lora_weights:
-        raise ValueError(f'can not find lora weight, the value is: {lora_weights}')
 
     load_8bit = args.load_8bit
-    if args.model == 'LLaMA-7B':
-        tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    if args.model in ['LLaMA-7B', 'LLaMA2-7B','LLaMA2-13B']:
+        token = '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'
+        tokenizer = LlamaTokenizer.from_pretrained(base_model, use_auth_token = True)
     else:
         tokenizer = AutoTokenizer.from_pretrained(base_model)
+
     if device == "cuda":
-        model = AutoModelForCausalLM.from_pretrained(
+        # model = AutoModelForCausalLM.from_pretrained(
+        #     base_model,
+        #     load_in_8bit=load_8bit,
+        #     torch_dtype=torch.float16,
+        #     device_map="auto",
+        #     trust_remote_code=True,
+        # ) 
+        model = LlamaForCausalLM.from_pretrained(
             base_model,
-            load_in_8bit=load_8bit,
             torch_dtype=torch.float16,
             device_map="auto",
-            trust_remote_code=True,
-        ) # fix zwq
-        # model = PeftModel.from_pretrained(
-        #     model,
-        #     lora_weights,
-        #     torch_dtype=torch.float16,
-        #     device_map={"":0}
-        # )
-    elif device == "mps":
-        model = AutoModelForCausalLM.from_pretrained(
-            base_model,
-            device_map={"": device},
-            torch_dtype=torch.float16,
-        )
-        model = PeftModel.from_pretrained(
-            model,
-            lora_weights,
-            device_map={"": device},
-            torch_dtype=torch.float16,
+            use_auth_token = True
         )
     else:
         model = AutoModelForCausalLM.from_pretrained(
             base_model, device_map={"": device}, low_cpu_mem_usage=True
-        )
-        model = PeftModel.from_pretrained(
-            model,
-            lora_weights,
-            device_map={"": device},
         )
 
         # unwind broken decapoda-research config
@@ -266,24 +214,16 @@ def load_model(args) -> tuple:
 
     return tokenizer, model
 
-
-def load_instruction(args) -> str:
-    instruction = ''
-    if not instruction:
-        raise ValueError('instruct not initialized')
-    return instruction
-
-
 def extract_answer_number(args, sentence: str) -> float:
     dataset = args.dataset.lower()
-    if dataset in ["multiarith", "addsub", "singleeq", "gsm8k", "svamp"]:
-        sentence = sentence.replace(',', '')
-        pred = [s for s in re.findall(r'-?\d+\.?\d*', sentence)]
-        if not pred:
-            return float('inf')
-        pred_answer = float(pred[-1])
-    else:
-        raise NotImplementedError(' not support dataset: {}'.format(dataset))
+    assert dataset in ["multiarith", "addsub", "singleeq", "gsm8k", "svamp"]
+
+    sentence = sentence.replace(',', '')
+    pred = [s for s in re.findall(r'-?\d+\.?\d*', sentence)]
+    if not pred:
+        return float('inf')
+    pred_answer = float(pred[-1])
+
     if isinstance(pred_answer, str):
         try:
             pred_answer = float(pred_answer)
@@ -291,17 +231,13 @@ def extract_answer_number(args, sentence: str) -> float:
             pred_answer = float('inf')
     return pred_answer
 
-
 def extract_answer_letter(args, sentence: str) -> str:
     sentence_ = sentence.strip()
     pred_answers = re.findall(r'A|B|C|D|E', sentence_)
     if pred_answers:
-        if not pred_answers:
-            return ''
         return pred_answers[0]
     else:
         return ''
-
 
 if __name__ == "__main__":
     fire.Fire(main)
